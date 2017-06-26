72aa14d8a7f0b21cd5b7edb4e318c83dcbcb9346
Bluebird2121 <baranenkoleg@gmai.com>
2017-06-26 22:29:49 +0300

    added pattern validation

--- src/main/java/com/bluebird/contacts/controllers/ContactsCtrl.java
+++ src/main/java/com/bluebird/contacts/controllers/ContactsCtrl.java
@@ -2,25 +2,23 @@ package com.bluebird.contacts.controllers;
 
 import com.bluebird.contacts.dtos.Contacts;
 import com.bluebird.contacts.services.ContactsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class ContactsCtrl {
 
-    @Autowired
-    private ContactsService contactsService;
-
     @RequestMapping(value = "contacts", method=RequestMethod.GET)
     public @ResponseBody
-    Contacts filterContacts(@RequestParam(value="nameFilter") String nameFilter) {
-        return contactsService.filter(nameFilter);
+    Contacts filterContacts(@RequestParam(value="nameFilter") String regexp, ContactsService contactsService) {
+        try {
+            return contactsService.filter(Pattern.compile(regexp));
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException(String.format("Filter value '%s' is invalid.", regexp));
+        }
     }
 
 }
