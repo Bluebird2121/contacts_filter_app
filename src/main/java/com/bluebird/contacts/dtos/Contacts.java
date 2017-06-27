b0ae932b0c78f066d66847ff9dc73961edd3be17
oleg.baranenko@swanteams.com <oleg.baranenko@swanteams.com>
2017-06-27 23:01:51 +0300

    +simple pagination filter

--- src/main/java/com/bluebird/contacts/dtos/Contacts.java
+++ src/main/java/com/bluebird/contacts/dtos/Contacts.java
@@ -3,10 +3,15 @@ package com.bluebird.contacts.dtos;
 import com.bluebird.contacts.domain.entity.Contact;
 
 import java.util.Collection;
+import java.util.List;
 
 public class Contacts {
 
     private Collection<Contact> contacts;
+
+    public Contacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
 
     public Collection<Contact> getContacts() {
         return contacts;
--- src/main/java/com/bluebird/contacts/services/impl/ContactsServiceImpl.java
+++ src/main/java/com/bluebird/contacts/services/impl/ContactsServiceImpl.java
@@ -4,15 +4,18 @@ import com.bluebird.contacts.domain.entity.Contact;
 import com.bluebird.contacts.domain.repository.ContactRepository;
 import com.bluebird.contacts.dtos.Contacts;
 import com.bluebird.contacts.services.ContactsService;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ContactsServiceImpl implements ContactsService {
+
+    private static final boolean IS_PARALLELED = false;
+    private static final int CONTACTS_PER_PAGE = 1;
 
     private final ContactRepository contactRepository;
 
@@ -23,11 +26,13 @@ public class ContactsServiceImpl implements ContactsService {
     @Override
     public Contacts filter(int page, Pattern nameFilter) {
         Iterable<Contact> allContacts = contactRepository.findAll();
-        Contacts result = new Contacts();
 
-        Collection<Contact> contacts = new ArrayList<>();
-        CollectionUtils.addAll(contacts, allContacts);
-        result.setContacts(contacts);
-        return result;
+        List<Contact> resultContacts = StreamSupport.stream(allContacts.spliterator(), IS_PARALLELED)
+                .filter(x -> ! nameFilter.matcher(x.getName()).find())
+                .skip(page * CONTACTS_PER_PAGE)
+                .limit(CONTACTS_PER_PAGE)
+                .collect(Collectors.toList());
+
+        return new Contacts(resultContacts);
     }
 }
