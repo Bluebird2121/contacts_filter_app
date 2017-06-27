f551706535d5ac17c7dc9b8da34b80a9a64dac2b
oleg.baranenko@swanteams.com <oleg.baranenko@swanteams.com>
2017-06-27 22:05:06 +0300

    person id type changed according to task

--- src/main/java/com/bluebird/contacts/domain/entity/Contact.java
+++ src/main/java/com/bluebird/contacts/domain/entity/Contact.java
@@ -10,15 +10,15 @@ public class Contact {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
 
     private String name;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
