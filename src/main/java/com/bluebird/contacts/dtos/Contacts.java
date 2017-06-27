341863b597a623603a1cf3d4f8241ffb3bf37f21
oleg.baranenko@swanteams.com <oleg.baranenko@swanteams.com>
2017-06-27 23:47:53 +0300

    +pagination info +empty contacts

--- src/main/java/com/bluebird/contacts/dtos/Contacts.java
+++ src/main/java/com/bluebird/contacts/dtos/Contacts.java
@@ -1,13 +1,21 @@
 package com.bluebird.contacts.dtos;
 
 import com.bluebird.contacts.domain.entity.Contact;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Contacts {
 
+    private static final Contacts EMPTY = new Contacts(Collections.emptyList());
+
     private Collection<Contact> contacts;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private PaginationInfo pagination;
+
 
     public Contacts(List<Contact> contacts) {
         this.contacts = contacts;
@@ -19,5 +27,17 @@ public class Contacts {
 
     public void setContacts(Collection<Contact> contacts) {
         this.contacts = contacts;
+    }
+
+    public PaginationInfo getPagination() {
+        return pagination;
+    }
+
+    public void setPagination(PaginationInfo pagination) {
+        this.pagination = pagination;
+    }
+
+    public static Contacts empty() {
+        return EMPTY;
     }
 }
