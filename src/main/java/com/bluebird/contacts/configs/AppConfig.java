ddb85a4e028ec34cdf967e43a1d386f23adce148
Bluebird2121 <baranenkoleg@gmai.com>
2017-06-30 22:24:52 +0300

    +session factory

--- src/main/java/com/bluebird/contacts/configs/AppConfig.java
+++ src/main/java/com/bluebird/contacts/configs/AppConfig.java
@@ -3,12 +3,17 @@ package com.bluebird.contacts.configs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "com.bluebird.contacts.controllers",
+        "com.bluebird.contacts.domain.repository",
         "com.bluebird.contacts.services"})
 @EntityScan("com.bluebird.contacts.domain.entity")
 @EnableJpaRepositories( "com.bluebird.contacts.domain.repository")
@@ -16,6 +21,13 @@ public class AppConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
+    }
+
+    @Bean
+    public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+        HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+        factory.setEntityManagerFactory(emf);
+        return factory;
     }
 
 }
