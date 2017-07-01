2504ecc5a2490b83a8c79479dff54805d90c6d1b
Bluebird2121 <baranenkoleg@gmai.com>
2017-07-01 13:03:26 +0300

    deleted session factory bean

--- src/main/java/com/bluebird/contacts/configs/AppConfig.java
+++ src/main/java/com/bluebird/contacts/configs/AppConfig.java
@@ -3,12 +3,8 @@ package com.bluebird.contacts.configs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
-
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -21,13 +17,6 @@ public class AppConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
-    }
-
-    @Bean
-    public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
-        HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
-        factory.setEntityManagerFactory(emf);
-        return factory;
     }
 
 }
