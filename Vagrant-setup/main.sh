15fd8ed263d17c9b257fd67d2a1b67d636d98be6
Bluebird2121 <baranenkoleg@gmai.com>
2017-07-03 00:32:26 +0300

    changed vagrant box because some strange key behavior

--- Vagrant-setup/main.sh
+++ Vagrant-setup/main.sh
@@ -1,12 +1,2 @@
-sudo apt-get update
-
-echo "Installing Java 8.."
-oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
-echo "Setting environment variables for Java 8.."
-sudo apt-get install -y oracle-java8-set-default
-
 echo "Installing Maven..."
 sudo apt-get install -y maven
--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,7 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "ubuntu/trusty32"
+  config.vm.box = "canimus/ubuntu-java8"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
