c3b9539a92d56a21c4cc9ada7fcbe16344d5939f
Bluebird2121 <baranenkoleg@gmai.com>
2017-07-02 23:43:13 +0300

    added response when db populated

---
+++ src/main/java/com/bluebird/contacts/dtos/SuccessDto.java
@@ -0,0 +1,14 @@
+package com.bluebird.contacts.dtos;
+
+public class SuccessDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
