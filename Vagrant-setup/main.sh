a51c982f85d5f71ed1f0b026953fc523d1866dc8
Bluebird2121 <baranenkoleg@gmai.com>
2017-07-03 10:05:49 +0300

    added gpg key

--- Vagrant-setup/main.sh
+++ Vagrant-setup/main.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 
 echo "Installing Java 8.."
+gpg --keyserver hkp://pgp.mit.edu  --recv-keys C2518248EEA14886
 oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
